e8c8d13478c70ca9d0e8354019c686a519f61ee8
jeanramalho <jeanramalho.dev@gmail.com>
2022-08-01 21:29:57 -0300

    Acerta requisicao e renderiza no front

--- js/script.js
+++ js/script.js
@@ -14,9 +14,22 @@ function main() {
             url: `https://api-sa-east-1.hygraph.com/v2/cl6aycg090i2k01umeyl23b40/master?query=query${getAllChamados}`,
         })
         .done(function(res) {
-            let data = res.data
+            let data = res.data.chamados
+            let div = document.getElementById('cont')
 
-            console.log(res.data)
+            data.map(item => {
+                output = `
+              <div class="card" id="card">
+                <h2 class="title" id="title">${item.title}</h2>
+                <p class="desc" id="desc">${item.description}</p>
+                <h4 class="date" id="date">${item.date}</h4>
+              </div>
+              `
+                console.log(output)
+                div.innerHTML += output
+
+            })
+
         })
 
 }
