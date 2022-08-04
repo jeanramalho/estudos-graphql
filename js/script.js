b7ab2217358d30123a58204b41f2313c8626e3d7
jeanramalho <jeanramalho.dev@gmail.com>
2022-08-04 14:05:26 -0300

    finaliza estudos do metodo get no graphql com ajax e vanilla js

--- js/script.js
+++ js/script.js
@@ -38,72 +38,7 @@ function main() {
 
 main()
 
-function postTest() {
-  //   let postando = `{createChamado(data: { title: "Face Mask", description: "face-mask" }) 
-  //   {title 
-  //   description}}
-  // }`
-    
-    // var url = "https://api-sa-east-1.hygraph.com/v2/cl6aycg090i2k01umeyl23b40/master?";
 
-    // var xhr = new XMLHttpRequest();
-    // xhr.open("POST", url);
-
-    // xhr.setRequestHeader("Content-Type", "application/json");
-
-    // xhr.onreadystatechange = function () {
-    //   if (xhr.readyState === 4) {
-    //       console.log(xhr.status);
-    //       console.log(xhr.responseText);
-    //   }};
-
-    // var data = `{"mutation{createChamado(data: { title: \"Jeanzão\", description: \"face-mask\" }) \n    {title \n    description}}","variables":null}
-    // `;
-
-    // xhr.send({data});
-
-    // var url = `https://api-sa-east-1.hygraph.com/v2/cl6aycg090i2k01umeyl23b40/master? `
-    // var dados = `{"query":"mutation{createChamado(data: { title: \"Jeanzão\", description: \"face-mask\" }) \n    {title \n    description}}","variables":null}`
-
-   
-    // fetch(url, {
-    //   method: "POST",
-    //   headers: {
-    //     "Content-Type": "application/json",
-    //     "Accept": "application/json"
-    //   },
-    //   body: JSON.stringify({dados})
-    // }).then(response => {
-    //   console.log(response.json())
-    //   return response.json()
-    // }).then(data => {
-    //   console.log(data)
-    // }).catch(error => {
-    //   console.log(error)
-    //})
-
-    $.ajax({
-      "executor": 0,
-      "method": "POST",
-      "url": "https://api-sa-east-1.hygraph.com/v2/cl6aycg090i2k01umeyl23b40/master?",
-      "body": JSON.stringify("{\"query\":\"mutation{createChamado(data: { title: \\\"Jeanzão\\\", description: \\\"face-mask\\\" }) \\n    {title \\n    description}}\",\"variables\":null}"),
-      "contentType": 0,
-      "headers": [],
-      "auth": {
-          "type": 0,
-          "params": {}
-      }
-  })
-  .done(function(res){
-    console.log(res)
-  })
-  .fail(function(error){
-    console.log(error)
-  })
-
-
-
-  }
 
       
     
